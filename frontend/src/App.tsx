d7676f290ec10a06123fbd07e6714635538ef8fe
thek4n <thek4n@yandex.ru>
2025-03-20 15:40:06 +0300

    frontend refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import type { Component } from 'solid-js';
-import { createSignal } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
 
 
 async function shortenUrl(url: string, disposable: number, ttl: string): Promise<string> {
@@ -7,7 +7,7 @@ async function shortenUrl(url: string, disposable: number, ttl: string): Promise
     throw new Error("Error: disposable counter cant be more then 255")
   }
 
-  const response = await fetch(`http://localhost:8080/?url=true&ttl=${ttl}&disposable=${disposable}`,
+  const response = await fetch(`http://paste.thek4n.ru/?url=true&ttl=${ttl}&disposable=${disposable}`,
     {
       method: "POST",
       mode: "cors",
@@ -28,6 +28,9 @@ async function shortenUrl(url: string, disposable: number, ttl: string): Promise
   return await response.text()
 };
 
+function copyToClipboard(text: string) {
+  navigator.clipboard.writeText(text).then(() => {alert("Copied")})
+}
 
 const App: Component = () => {
   const [disposableCounter, setDisposableCounter] = createSignal<number>(0);
@@ -36,11 +39,19 @@ const App: Component = () => {
 
 
   const setShortened = async (url: string) => {
-    const answer = await shortenUrl(url, disposableCounter(), expirationTime())
+    if (!url.startsWith("http://") || !url.startsWith("https://")) {
+      url = "https://" + url
+    }
+
+    try {
+      const answer = await shortenUrl(url, disposableCounter(), expirationTime())
     setShortenedURL(answer)
+    } catch (e) {
+      alert("Error")
+    }
   };
 
-  const [shortenedURL, setShortenedURL] = createSignal<string>();
+  const [shortenedURL, setShortenedURL] = createSignal<string>("");
 
   return (
     <div>
@@ -89,11 +100,28 @@ const App: Component = () => {
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >Shorten</button>
 
-        <div
-          class="text-white m-2"
-        >
-          {shortenedURL()}
-        </div>
+        <Show when={shortenedURL()}>
+          <div
+            class="text-white"
+          >
+            <div class="mt-10">
+              <div class="flex items-center border border-gray-700 rounded-lg overflow-hidden">
+                <input
+                  type="text"
+                  value={shortenedURL()}
+                  readonly
+                  class="flex-grow px-4 py-2 outline-none bg-black-50 bg-gray-800"
+                />
+                <button
+                  class="bg-blue-500 text-white px-4 py-2 hover:bg-blue-600 transition-colors"
+                  onclick={() => copyToClipboard(shortenedURL())}
+                >
+                  Копировать
+                </button>
+              </div>
+            </div>
+          </div>
+        </Show>
       </header>
     </div>
   );
