f5e2cab60306f0c10db25693e8d04bf6d6f355ac
thek4n <thek4n@yandex.ru>
2025-06-20 20:17:28 +0300

    rm unneeded go generate and coverage profile

--- make.sh
+++ make.sh
@@ -5,8 +5,7 @@ set -ue
 
 test() {
     export GOMAXPROCS=1
-    go test -tags integration,e2e,e2ettl -count=1 -race -cover -coverprofile=/tmp/coverage.out -covermode=atomic ./... && \
-    go tool cover -func=/tmp/coverage.out
+    go test -tags integration,e2e,e2ettl -count=1 -race -cover -covermode=atomic ./...
 }
 
 build() {
